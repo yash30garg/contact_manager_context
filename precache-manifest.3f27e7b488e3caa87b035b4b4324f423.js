f01c1cf14b5578d0845de99126485ce95fa581ea
yash.garg <yash.garg@adpushup.com>
2019-08-26 01:04:53 +0530

    Updates

---
+++ precache-manifest.3f27e7b488e3caa87b035b4b4324f423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1253f2fdc3dc135a791b6bd6780aa350",
+    "url": "/contact_manager_context/index.html"
+  },
+  {
+    "revision": "2bc6b3243719cfd61f28",
+    "url": "/contact_manager_context/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bbd5353e72ce75792d7f",
+    "url": "/contact_manager_context/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2bc6b3243719cfd61f28",
+    "url": "/contact_manager_context/static/js/2.2a270030.chunk.js"
+  },
+  {
+    "revision": "bbd5353e72ce75792d7f",
+    "url": "/contact_manager_context/static/js/main.17ce7c4b.chunk.js"
+  },
+  {
+    "revision": "8f003a63a125136f2346",
+    "url": "/contact_manager_context/static/js/runtime~main.80e96b13.js"
+  }
+]);
